2d4f8043d46b3e734d04eff5eb943e09129dce8c
Rob Wood <rdrwood@gmail.com>
2018-08-13 14:20:10 -0700

    class worok 81318

--- FileIOSample/Form1.Designer.cs
+++ FileIOSample/Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAddCourse = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoadSchedule = new System.Windows.Forms.Button();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.lstSchedule = new System.Windows.Forms.ListBox();
+            this.lblEnterCourse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAddCourse
+            // 
+            this.btnAddCourse.Location = new System.Drawing.Point(55, 71);
+            this.btnAddCourse.Name = "btnAddCourse";
+            this.btnAddCourse.Size = new System.Drawing.Size(218, 23);
+            this.btnAddCourse.TabIndex = 0;
+            this.btnAddCourse.Text = "Add Course";
+            this.btnAddCourse.UseVisualStyleBackColor = true;
+            this.btnAddCourse.Click += new System.EventHandler(this.btnAddCourse_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(312, 50);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save Schedule";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoadSchedule
+            // 
+            this.btnLoadSchedule.Location = new System.Drawing.Point(312, 113);
+            this.btnLoadSchedule.Name = "btnLoadSchedule";
+            this.btnLoadSchedule.Size = new System.Drawing.Size(112, 23);
+            this.btnLoadSchedule.TabIndex = 2;
+            this.btnLoadSchedule.Text = "Load Schedule";
+            this.btnLoadSchedule.UseVisualStyleBackColor = true;
+            this.btnLoadSchedule.Click += new System.EventHandler(this.btnLoadSchedule_Click);
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(173, 32);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(100, 20);
+            this.txtClass.TabIndex = 3;
+            // 
+            // lstSchedule
+            // 
+            this.lstSchedule.FormattingEnabled = true;
+            this.lstSchedule.Location = new System.Drawing.Point(55, 113);
+            this.lstSchedule.Name = "lstSchedule";
+            this.lstSchedule.Size = new System.Drawing.Size(218, 95);
+            this.lstSchedule.TabIndex = 4;
+            // 
+            // lblEnterCourse
+            // 
+            this.lblEnterCourse.AutoSize = true;
+            this.lblEnterCourse.Location = new System.Drawing.Point(52, 38);
+            this.lblEnterCourse.Name = "lblEnterCourse";
+            this.lblEnterCourse.Size = new System.Drawing.Size(71, 13);
+            this.lblEnterCourse.TabIndex = 5;
+            this.lblEnterCourse.Text = "Enter Course;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(499, 278);
+            this.Controls.Add(this.lblEnterCourse);
+            this.Controls.Add(this.lstSchedule);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.btnLoadSchedule);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddCourse);
+            this.Name = "Form1";
+            this.Text = "Schedule Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAddCourse;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoadSchedule;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.ListBox lstSchedule;
+        private System.Windows.Forms.Label lblEnterCourse;
     }
 }
 
--- FileIOSample/Form1.cs
+++ FileIOSample/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,77 @@ namespace FileIOSample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAddCourse_Click(object sender, EventArgs e)
+        {
+            lstSchedule.Items.Add(txtClass.Text);
+            txtClass.Clear();
+            txtClass.Focus();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // SaveScheduleBasic();
+            SaveSchedule();
+            MessageBox.Show("Data was saved");
+        }
+        private void SaveSchedule()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Data File |*.data:";
+            save.RestoreDirectory = true;
+            if(save.ShowDialog() == DialogResult.OK)
+            {
+                string path = save.FileName;
+                if (path == string.Empty)
+                    return;
+                StringBuilder courseList = GetCoursesFromListBox();
+                File.WriteAllText(path, courseList.ToString());
+            }
+        }
+        private void SaveScheduleBasic()
+        {
+            StringBuilder data = GetCoursesFromListBox();
+            string folder = GetMyDocumentsPath();
+            //string path = @"c:\test.txt";
+            string path = folder + "\\test.data";
+            File.WriteAllText(path, data.ToString());
+        }
+
+        private StringBuilder GetCoursesFromListBox()
+        {
+            StringBuilder data = new StringBuilder();
+            for (int i = 0; i < lstSchedule.Items.Count; i++)
+            {
+                data.AppendLine(lstSchedule.Items[i].ToString());
+            }
+
+            return data;
+        }
+
+        private void btnLoadSchedule_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = GetMyDocumentsPath();
+            
+            fileDialog.Filter = "Data File |*.data";
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // read the file
+                List<string> courses = File.ReadLines(fileDialog.FileName).ToList();
+                lstSchedule.Items.Clear();
+                foreach (string course in courses)
+                {
+                    lstSchedule.Items.Add(course);
+                }
+                
+            }
+        }
+
+        private string GetMyDocumentsPath()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         }
     }
 }
